0e96fa9fc6060eccae252349f5ef331eceb4ab99
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2025-05-29 11:13:52 +0530

    Header Added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,94 @@
+function Header() {
+    return (
+      <>
+        <div className="header_container">
+          <div className="header_firstpart">
+            <a href="#">NEW IN</a>
+            <a href="#">FREE UK Delivery Over $65*</a>
+            <a href="#">BULK BUY OPTIONS AVAILABLE</a>
+          </div>
+          <div className="header_secondpart">
+            <div className="container">
+              <div className="social_icon_container">
+                <img
+                  src="//evengreener.com/cdn/shop/files/bxl-instagram_375x375.png?v=1681291645"
+                  loading="lazy"
+                  data-src="//evengreener.com/cdn/shop/files/bxl-instagram_375x375.png?v=1681291645"
+                  data-srcset="//evengreener.com/cdn/shop/files/bxl-instagram_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-instagram_750x750.png?v=1681291645 2x"
+                  data-lazy-load=""
+                  alt="Instagram"
+                  width="32"
+                  height="32"
+                  data-alt="Instagram"
+                  class="loaded"
+                  srcset="//evengreener.com/cdn/shop/files/bxl-instagram_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-instagram_750x750.png?v=1681291645 2x"
+                  data-was-processed="true"
+                />
+                <img
+                  src="//evengreener.com/cdn/shop/files/bxl-youtube_375x375.png?v=1681291645"
+                  loading="lazy"
+                  data-src="//evengreener.com/cdn/shop/files/bxl-youtube_375x375.png?v=1681291645"
+                  data-srcset="//evengreener.com/cdn/shop/files/bxl-youtube_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-youtube_750x750.png?v=1681291645 2x"
+                  data-lazy-load=""
+                  alt="image"
+                  width="32"
+                  height="32"
+                  data-alt=""
+                  class="loaded"
+                  srcset="//evengreener.com/cdn/shop/files/bxl-youtube_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-youtube_750x750.png?v=1681291645 2x"
+                  data-was-processed="true"
+                ></img>
+                <img
+                  src="//evengreener.com/cdn/shop/files/bxl-facebook_375x375.png?v=1681291645"
+                  loading="lazy"
+                  data-src="//evengreener.com/cdn/shop/files/bxl-facebook_375x375.png?v=1681291645"
+                  data-srcset="//evengreener.com/cdn/shop/files/bxl-facebook_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-facebook_750x750.png?v=1681291645 2x"
+                  data-lazy-load=""
+                  alt="Facebook"
+                  width="32"
+                  height="32"
+                  data-alt="Facebook"
+                  class="loaded"
+                  srcset="//evengreener.com/cdn/shop/files/bxl-facebook_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-facebook_750x750.png?v=1681291645 2x"
+                  data-was-processed="true"
+                ></img>
+                <img
+                  src="//evengreener.com/cdn/shop/files/bxl-pinterest_375x375.png?v=1681291645"
+                  loading="lazy"
+                  data-src="//evengreener.com/cdn/shop/files/bxl-pinterest_375x375.png?v=1681291645"
+                  data-srcset="//evengreener.com/cdn/shop/files/bxl-pinterest_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-pinterest_750x750.png?v=1681291645 2x"
+                  data-lazy-load=""
+                  alt="Pinterest"
+                  width="32"
+                  height="32"
+                  data-alt="Pinterest"
+                  class="loaded"
+                  srcset="//evengreener.com/cdn/shop/files/bxl-pinterest_375x375.png?v=1681291645 1x, //evengreener.com/cdn/shop/files/bxl-pinterest_750x750.png?v=1681291645 2x"
+                  data-was-processed="true"
+                ></img>
+              </div>
+                        <div className="logo">
+                        <img srcset="//evengreener.com/cdn/shop/files/EG-Stacked-Primary_1_1_85x.png?v=1641822672 1x, //evengreener.com/cdn/shop/files/EG-Stacked-Primary_1_1_170x.png?v=1641822672 2x" alt="Site logo" width="151" height="69" loading="lazy"/>
+              </div>
+              <div className="search_container">
+                <input type="text" />
+              </div>
+                    </div>
+                    <div className="navigation_container">
+                        <ul>
+                            <li>GARDEN COMPOSTING</li>
+                            <li>FOOD WASTE COMPOSTING</li>
+                            <li>WATER SAVING</li>
+                            <li>GARDENING</li>
+                            <li>BRANDS</li>
+                        </ul>
+                    </div>
+          </div>
+                <div className="header_thirdpart">
+                    <p>20+ Years Trading Online</p>
+          </div>
+        </div>
+      </>
+    );
+}
+export default Header;
